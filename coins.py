e3171252ae76a2055b8810ebddb9ce1ea6b6e0c2
pspriyasharma <111630086+pspriyasharma@users.noreply.github.com>
2022-08-20 12:51:32 +0530

    Add files via upload

---
+++ coins.py
@@ -0,0 +1,38 @@
+"""
+Stores coin definitions
+"""
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Coin:
+    value: int
+    label: str
+
+    def __str__(self):
+        return self.label
+
+
+@dataclass(frozen=True)
+class Nickel(Coin):
+    value: int = 5
+    label: str = '5¢'
+
+
+@dataclass(frozen=True)
+class Dime(Coin):
+    value: int = 10
+    label: str = '10¢'
+
+
+@dataclass(frozen=True)
+class Quarter(Coin):
+    value: int = 25
+    label: str = '25¢'
+
+
+@dataclass(frozen=True)
+class Pennie(Coin):
+    value: int = 100
+    label: str = '$1'
+
+
